1192c11a90c9ec668751fe46032df9e440c1dc3c
truongnhatanh7 <truongnhatanhw@gmail.com>
2023-06-01 14:35:27 +0700

    feat: add modal

--- app/layout.tsx
+++ app/layout.tsx
@@ -2,6 +2,7 @@ import './globals.css'
 import { Inter } from 'next/font/google'
 import { Nunito } from 'next/font/google'
 import { Navbar } from '@/app/components/Navbar'
+import { RegisterModal } from './components/Modal/RegisterModal'
 
 const inter = Inter({ subsets: ['latin'] })
 const font = Nunito({
@@ -21,6 +22,7 @@ export default function RootLayout({
   return (
     <html lang="en">
       <body className={font.className}>
+        <RegisterModal />
         <Navbar />
         {children}
       </body>
