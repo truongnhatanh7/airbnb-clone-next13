19ee01899d24dbc7f0eb8d77154623ed79ac822c
truongnhatanh7 <truongnhatanhw@gmail.com>
2023-06-02 16:56:47 +0700

    feat: add login functionality

--- app/components/Navbar/UserMenu/index.tsx
+++ app/components/Navbar/UserMenu/index.tsx
@@ -8,6 +8,7 @@ import { MenuItem } from '../MenuItem'
 import { useRegisterModal } from '@/app/hooks/useRegisterModal'
 import { useLoginModal } from '@/app/hooks/useLoginModal'
 import { User } from '@prisma/client'
+import { signOut } from 'next-auth/react'
 
 interface UserMenuProps {
   currentUser?: User | null;
@@ -84,16 +85,49 @@ export const UserMenu: React.FC<UserMenuProps> = ({
               flex-col
               cursor-pointer
             ">
-              <>
-                <MenuItem
-                  onClick={loginModal.onOpen}
-                  label="Login"
-                />
-                <MenuItem
-                  onClick={registerModal.onOpen}
-                  label="Sign Up"
-                />
-              </>
+              {
+                currentUser ? (
+                  <>
+                    <MenuItem
+                      onClick={() => { }}
+                      label="My trips"
+                    />
+                    <MenuItem
+                      onClick={() => { }}
+                      label="My favorites"
+                    />
+                    <MenuItem
+                      onClick={() => { }}
+                      label="My reservations"
+                    />
+                    <MenuItem
+                      onClick={() => { }}
+                      label="My properties"
+                    />
+                    <MenuItem
+                      onClick={() => { }}
+                      label="Airbnb my home"
+                    />
+                    <hr/>
+                    <MenuItem
+                      onClick={() => signOut()}
+                      label="Logout"
+                    />
+                  </>
+                ) : (
+                  <>
+                    <MenuItem
+                      onClick={loginModal.onOpen}
+                      label="Login"
+                    />
+                    <MenuItem
+                      onClick={registerModal.onOpen}
+                      label="Sign Up"
+                    />
+                  </>
+                )
+              }
+
             </div>
           </div>
         )
