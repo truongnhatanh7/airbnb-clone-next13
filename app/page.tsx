f5eca54ca78f9e78439e672995fae2cb081c9c19
truongnhatanh7 <truongnhatanhw@gmail.com>
2023-06-06 14:11:51 +0700

    feat: add empty state

--- app/page.tsx
+++ app/page.tsx
@@ -1,7 +1,36 @@
 import Image from 'next/image'
+import { ClientOnly } from './components/ClientOnly'
+import { Container } from './components/Container'
+import { EmptyState } from './components/EmptyState';
 
 export default function Home() {
+  const isEmpty = true;
+
+  if (isEmpty) {
+    return (
+      <ClientOnly>
+        <EmptyState />
+      </ClientOnly>
+    )
+  }
+
   return (
-    <></>
+    <ClientOnly>
+      <Container>
+        <div className="
+          pt-24
+          grid
+          grid-cols-1
+          sm:grid-cols-2
+          md:grid-cols-3
+          lg:grid-cols-4
+          xl:grid-cols-5
+          2xl:grid-cols-6
+          gap-8
+        ">
+          <div className=""></div>
+        </div>
+      </Container>
+    </ClientOnly>
   )
 }
